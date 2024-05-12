6f8633697a7d9e35c28c9a6c9441ebc1f1aacb7e
deysur <surajitdey000@gmail.com>
2024-05-12 15:34:04 +0530

    add demoprod

--- src/main/java/com/example/demo/demoprod.java
+++ src/main/java/com/example/demo/demoprod.java
@@ -7,11 +7,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class demoprod {
     @GetMapping("/demoprod")
-    public String getData() {
-        return "This is my prod branch";
-    }
+    public String getData() { return "This is my prod branch"; }
 }
